2b765cdfd875b92e57acc948557ce7115076970b
Muhammadikhsandev <mikhsan102002@gmail.com>
2022-12-19 11:30:52 +0700

    folder baru

--- .KASIR/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('#toggle').onclick = () => {
-    let el = document.querySelector('#wrapper').classList;
-    if (el.contains('hide')) {
-        el.remove('hide')
-    }else {
-        el.add('hide')
-    }
-}
